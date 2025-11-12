a039e258c1f5bed6ef363427bbdb7afff098fb00
Farheen <farheensaba2006@gmail.com>
2025-11-12 16:52:44 +0545

    Game

--- evenodd.js
+++
@@ -1,7 +0,0 @@
-let num=Number(prompt("Enter Number:"));
-if(num%2==0){
-    console.log("Even");
-}
-else{
-    console.log("Odd");
-}
--- fact.js
+++
@@ -1,8 +0,0 @@
-function fact(num){
-    if(num==0)
-        return 1;
-    else 
-        return num*fact(num-1);
-}
-let Factorial=fact(10);
-console.log(Factorial);
